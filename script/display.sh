d2f91d000b39416a6080ad863c94c58988095a25
pmcb55 <patm@inrupt.com>
2020-05-29 22:53:14 +0100

    Reorganised repo to allow LAG live in /bin, and scripts in their own directory.

--- script/display.sh
+++ script/display.sh
@@ -11,22 +11,28 @@ YELLOW=$(tput setaf 3)
 BLUE=$(tput setaf 4)
 NORMAL=$(tput sgr0)
 
+# Get the directory this script itself is located in.
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Default directory to target is a sibling directory of where this script is.
+TARGET_DIR="${SCRIPT_DIR}/../vocab"
+
 helpFunction() {
-    echo ""
-    printf "${BLUE}Usage: $0 [ -a -b -c -d -e ]\n"
+    printf "${BLUE}Usage: $0 [ -t <TargetDirectory> ] [ -a <LAG versions> | -b <Java-LVT versions> | -c <JavaScript_LVT versions> | -d <Generated Java versions> | -e <Generated JavaScript versions> ]\n"
     printf "Displays version numbers being used across LIT Artifact Generator configuration files.${NORMAL}\n"
     printf "${RED}NOTE: Also displays version numbers from commented-out entries, which will be prefixed with a hash '#'.${NORMAL}\n\n"
     printf "${BLUE}Options:${NORMAL}\n"
+    printf "\t-t ${YELLOW}Optional: ${BLUE}Target directory (default is: [${TARGET_DIR}])${NORMAL}\n"
     printf "\t-a ${BLUE}LIT Artifact Generator versions${NORMAL}\n"
     printf "\t-b ${BLUE}LIT Vocab Term Java versions${NORMAL}\n"
     printf "\t-c ${BLUE}LIT Vocab Term JavaScript versions${NORMAL}\n"
-    printf "\t-d ${BLUE}Generated artifact Java versions${NORMAL}\n"
-    printf "\t-e ${BLUE}Generated artifact JavaScript versions${NORMAL}\n\n"
+    printf "\t-d ${BLUE}Generated Java JAR versions${NORMAL}\n"
+    printf "\t-e ${BLUE}Generated JavaScript NPM versions${NORMAL}\n\n"
 }
 
-while getopts ":abcde" opt
+while getopts ":t:abcde" opt
 do
     case "$opt" in
+      t ) TARGET_DIR="$OPTARG" ;;
       a ) litArtifactGenerator=true ;;
       b ) litVocabTermJava=true ;;
       c ) litVocabTermJavaScript=true ;;
@@ -60,7 +66,7 @@ then
     # LIT Artifact Generator.
     # Alternative is to use 'find' first, but grep can handle what we need.
     #  command="find . -type f \( -name '*.yml' -o -name '*.yaml' \) -not -path \"*/Generated/*\" -exec grep \"artifactGeneratorVersion:\" {} +"
-    command="grep -r \"artifactGeneratorVersion:\" --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github,lit-artifact-generator*}"
+    command="grep -r \"artifactGeneratorVersion:\" ${TARGET_DIR} --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github}"
     printf "${RED}a) LIT Artifact Generator versions:${NORMAL}\n"
     echo $command | bash | sed 's/\s*artifactGeneratorVersion: //' | column -s ':' -t
     echo ""
@@ -69,7 +75,7 @@ fi
 if [ "${litVocabTermJava:-}" ]
 then
     # Java LIT Vocab Term versions.
-    command="grep -r \"litVocabTermVersion:\s*[0-9]\" --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github,lit-artifact-generator*}"
+    command="grep -r \"litVocabTermVersion:\s*[0-9]\" ${TARGET_DIR} --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github}"
     printf "${RED}b) Java LIT Vocab Term versions:${NORMAL}\n"
     echo $command | bash | sed 's/\s*litVocabTermVersion: //' | column -s ':' -t
     echo ""
@@ -78,7 +84,7 @@ fi
 if [ "${litVocabTermJavaScript:-}" ]
 then
     # JavaScript LIT Vocab Term versions.
-    command="grep -r \"litVocabTermVersion:\s*\\\"\^\" --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github,lit-artifact-generator*}"
+    command="grep -r \"litVocabTermVersion:\s*\\\"\^\" ${TARGET_DIR} --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github}"
     printf "${RED}c) JavaScript LIT Vocab Term versions:${NORMAL}\n"
     echo $command | bash | sed 's/\s*litVocabTermVersion: //' | column -s ':' -t
     echo ""
@@ -87,8 +93,8 @@ fi
 if [ "${artifactJava:-}" ]
 then
     # Java generated artifact versions.
-    command="grep -r \"artifactVersion:\s*[0-9]\" --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github,lit-artifact-generator*}"
-    printf "${RED}d) Java generated artifact versions:${NORMAL}\n"
+    command="grep -r \"artifactVersion:\s*[0-9]\" ${TARGET_DIR} --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github}"
+    printf "${RED}d) Generated Java JAR versions:${NORMAL}\n"
     echo $command | bash | sed 's/\s*artifactVersion: //' | column -s ':' -t
     echo ""
 fi
@@ -96,8 +102,8 @@ fi
 if [ "${artifactJavaScript:-}" ]
 then
     # JavaScript generated artifact versions.
-    command="grep -r \"artifactVersion:\s*\\\"\" --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github,lit-artifact-generator*}"
-    printf "${RED}e) JavaScript generated artifact versions:${NORMAL}\n"
+    command="grep -r \"artifactVersion:\s*\\\"\" ${TARGET_DIR} --include=*.{yml,yaml} --exclude-dir={Generated,node_modules,.github}"
+    printf "${RED}e) Generated JavaScript NPM versions:${NORMAL}\n"
     echo $command | bash | sed 's/\s*artifactVersion: //' | column -s ':' -t
     echo ""
 fi
