c0146059ed3ce9c744c69318f1f30a8a71a18a0f
pmcb55 <patm@inrupt.com>
2021-08-10 12:03:05 +0100

    Re-instate the rdflib term test - it works now

--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
+//
+// Note: This code simply imports some of our generated artifacts, and
+//  attempts to output to the console values from those artifacts. If you wish
+//  to test artifacts generated locally first (e.g., perhaps before pushing
+//  changes to GitHub), then you'll need to first run `npm install` pointing
+//  at your local repository, e.g., for Verdaccio on it's default port:
+//    npm install --registry http://localhost:4873
+//
+
 const { FOAF, RDF, RDF_INRUPT } = require("@inrupt/vocab-common-rdf-vocabterm");
 console.log(`Common RDF: FOAF vocab term accountServiceHomepage: [${FOAF.accountServiceHomepage}]`);
 console.log(`Common RDF: FOAF vocab term accountServiceHomepage - label: [${FOAF.accountServiceHomepage.label}]`);
@@ -21,9 +30,9 @@ const { SOLID:SOLID_RDFJS_DATAFACTORY } = require("@inrupt/vocab-solid-rdfjs-rdf
 console.log(`Solid vocab term ListedDocument (RDF/JS DataFactory): [${SOLID_RDFJS_DATAFACTORY.ListedDocument.value}]`);
 console.log();
 
-// const { SOLID:SOLID_RDFLIB } = require("@inrupt/vocab-solid-rdflib");
-// console.log(`Solid vocab term ListedDocument (rdflib): [${SOLID_RDFLIB.ListedDocument}]`);
-// console.log();
+const { SOLID:SOLID_RDFLIB } = require("@inrupt/vocab-solid-rdflib");
+console.log(`Solid vocab term ListedDocument (rdflib): [${SOLID_RDFLIB.ListedDocument}]`);
+console.log();
 
 
 // We rename the Inrupt Common vocab, just to indicate that it's coming in
