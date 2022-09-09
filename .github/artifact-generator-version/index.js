2747eb16f00c28edc7b874ca9db5eabd98cb4088
pmcb55 <patm@inrupt.com>
2022-09-09 15:07:04 +0100

    Updated to use latest yaml.js interfaces

--- .github/artifact-generator-version/index.js
+++ .github/artifact-generator-version/index.js
@@ -3,8 +3,8 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 try {
-  // `vocab-config-path` input defined in action metadata file
-  yamlConfiguration = yaml.safeLoad(fs.readFileSync(core.getInput('vocab-config-path'), 'utf8'));
+  // `vocab-config-path` input defined in action metadata file.
+  yamlConfiguration = yaml.load(fs.readFileSync(core.getInput('vocab-config-path'), 'utf8'));
   console.log(JSON.stringify(yamlConfiguration, null, "  "));
   core.setOutput("vocab-generator-version", yamlConfiguration.artifactGeneratorVersion);
 } catch (error) {
